7c303982b37a91167dae6bbae128de6037c6813c
Burlesco70 <mario.nardi.70@gmail.com>
2021-03-24 20:05:18 +0100

    basi

---
+++ 02/1_build_fn.py
@@ -0,0 +1,20 @@
+print("Ciao Python Biella Group 😀")
+print('Ciao Python Biella Group 😙')
+
+print("🐵" * 10)
+
+print(2+8)
+
+print(max(12,4,124,4534,3,44))
+
+print(min(12,4,124,4534,3,44))
+
+print("Che bello studiare Python".upper())
+
+print("Che bello studiare Python".lower())
+
+messaggio = "Il mio frutto preferito è la prugna"
+messaggio = messaggio.replace("prugna", "pera")
+print(messaggio)
+
+print(len(messaggio))
---
+++ 02/2_variabili.py
@@ -0,0 +1,13 @@
+# definizione delle variabili di gioco
+livello_gioco = 1
+titolo = "Il mio gioco"
+gioco_finito = False
+
+# stampa le variabili
+print(livello_gioco)
+print(titolo)
+print(gioco_finito)
+
+livello_gioco = 2
+
+print(livello_gioco)
